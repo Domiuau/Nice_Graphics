e60b8a4a271f3cadaa756035d3e37888760cacfc
eliana-eml <eml.eliana57@hotmail.com>
2025-04-12 21:09:00 -0300

    feat: tela de histórico de análises

--- front/nice-graphics/js/pages/header.js
+++ front/nice-graphics/js/pages/header.js
@@ -1,24 +1,45 @@
 function menuFlutuanteCliente() {
     const iconeCliente = document.querySelector('.bx-user');
     const header = document.querySelector('.header');
-    
-    iconeCliente.addEventListener('click', () => {
+    let menuAberto = false;
+    let menuElement = null;
 
-        header.innerHTML += `
-            <div class="opcoes-visualizacao-cliente">
+    iconeCliente.addEventListener('click', (event) => {
+        event.stopPropagation(); 
+
+        if (menuAberto) {
+            menuElement.remove();
+            menuAberto = false;
+        } else {
+            menuElement = document.createElement('div');
+            menuElement.classList.add('opcoes-visualizacao-cliente');
+            menuElement.innerHTML = `
                 <ul>
-                    <li title="Meu Cadastro" onclick="window.location.href = './perfil-usuario.html'">
+                    <li title="Meu Perfil" onclick="window.location.href = './perfil-usuario.html'">
                         <i class='bx bxs-user-detail'></i>
                         <p>Meu Perfil</p>
                     </li>
-                    <li title="Meus Pedidos" onclick="window.location.href = './historico-usuario.html'">
+                    <li title="Meu Histórico" onclick="window.location.href = './historico.html'">
                         <i class='bx bx-history'></i>
                         <p>Meu Histórico</p>
                     </li>
+                    <li title="Sair" onclick="window.location.href = './login.html'">
+                        <i class='bx bx-log-out'></i>
+                        <p>Sair</p>
+                    </li>
                 </ul>
-            </div>
-        `;
+            `;
+            header.appendChild(menuElement);
+            menuAberto = true;
+        }
+    });
 
+    // Fecha o menu ao clicar fora
+    document.addEventListener('click', (event) => {
+        if (menuAberto && !iconeCliente.contains(event.target) && !menuElement.contains(event.target)) {
+            menuElement.remove();
+            menuAberto = false;
+        }
     });
 }
 
