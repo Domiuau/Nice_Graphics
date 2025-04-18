9754756268e4e5845eac06a1d4e3383b80937338
eliana-eml <eml.eliana57@hotmail.com>
2025-04-18 16:18:35 -0300

    feat: header dinâmico conforme o status do usuário (logado ou deslogado)

--- front/nice-graphics/js/pages/header.js
+++ front/nice-graphics/js/pages/header.js
@@ -1,3 +1,44 @@
+window.onload = function definirHeaderUsuario() {
+
+    const opcoesUsuarioStatus = document.getElementById('opcoes-status-usuario');
+    let usuario = 1;
+
+    //FAZER O IF DO USUÁRIO ESTAR LOGADO OU NÃO
+
+    //Se ESTIVER logado faça
+    if (usuario == 0) {
+
+        opcoesUsuarioStatus.innerHTML = ``;
+        opcoesUsuarioStatus.innerHTML = `
+            <div class="icone-usuario">
+                <button class="btn-icone-visualizar-historico" title="Visualizar Histórico" onclick="window.location.href = '../pages/historico.html'"><i class='bx bx-history'></i></button>
+                <button class="btn-icone-usuario" title="Ver opções perfil"><i class='bx bx-user'></i></button>
+            </div>
+        `;
+
+        menuFlutuanteCliente();
+
+    } else { //Se NÃO ESTIVER logado faça
+        opcoesUsuarioStatus.innerHTML = ``;
+        opcoesUsuarioStatus.innerHTML = `
+            <div>
+                <ul class="navbar-header-buttons">
+                    <li>
+                        <button class="btn-entrar" onclick="window.location.href='../pages/login.html'">Entrar</button>
+                    </li>
+                    <li>
+                        <button class="btn-cadastro" onclick="window.location.href='../pages/login.html'">Cadastre-se</button>
+                    </li>
+                </ul>
+            </div>
+        `;
+    }
+
+
+
+}
+
+
 function menuFlutuanteCliente() {
     const iconeCliente = document.querySelector('.bx-user');
     const header = document.querySelector('.header');
@@ -19,11 +60,11 @@ function menuFlutuanteCliente() {
                         <i class='bx bx-user'></i>
                         <p>Nome Usuário</p>
                     </li>
-                    <li class="opcao-usuario" title="Meu Perfil" onclick="window.location.href = './perfil-usuario.html'">
+                    <li class="opcao-usuario" title="Meu Perfil" onclick="window.location.href = '../pages/perfil-usuario.html'">
                         <i class='bx bxs-user-detail'></i>
                         <p>Meu Perfil</p>
                     </li>
-                    <li class="opcao-usuario" title="Sair" onclick="window.location.href = './login.html'">
+                    <li class="opcao-usuario" title="Sair" onclick="window.location.href = '../pages/login.html'">
                         <i class='bx bx-log-out'></i>
                         <p>Sair</p>
                     </li>
@@ -43,4 +84,4 @@ function menuFlutuanteCliente() {
     });
 }
 
-menuFlutuanteCliente();
+
